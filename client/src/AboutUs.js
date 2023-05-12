659a536982478bb7f1c918cb037c5ea9f7b4c7c9
dakshveer-singh04 <dakshveersinghchauhan@gmail.com>
2023-05-13 00:47:52 +0530

    First commit

---
+++ client/src/AboutUs.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about">
+        <div class="bg"></div>
+        <h1 style={{ color: "white"}}>Decentralized Cloud Storage</h1>
+        <div>
+        <h1>About Us</h1>
+        <div className="member">
+            <h2>Harsh Chirag Patel</h2>
+            <p>(202001032)</p>
+        </div>
+        <div className="member">
+            <h2>Dakshveer Singh Chauhan</h2>
+            <p>(202001048)</p>
+        </div>
+        <div className="member">
+            <h2>Japan Bhatt</h2>
+            <p>(202001066)</p>
+        </div>
+        <div className="member">
+            <h2>Deep Shah</h2>
+            <p>(202001129)</p>
+        </div>
+        <div className="member">
+            <h2>Rohan Champaneri</h2>
+            <p>(202001414)</p>
+        </div>
+        <div className="member">
+            <h2>Jay Grover</h2>
+            <p>(202001467)</p>
+        </div>
+        
+        </div>
+    </div>
+    
+  );
+}
+
+export default AboutUs;
